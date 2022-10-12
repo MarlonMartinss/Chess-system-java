8541074082c1b0ea35475de2c2250b4aa5aa3eeb
MarlonMartinss <marlonsd783@gmail.com>
2022-10-12 20:13:28 -0300

    fix problems

--- src/application/UI.java
+++ src/application/UI.java
@@ -22,10 +22,7 @@ public class UI {
 		 else {
 			 System.out.println(piece);
 		 }
-<<<<<<< HEAD
-		 System.out.print("  ");
-=======
-		 System.out.print(" ");
->>>>>>> 1c1fd794c391092f96a9df90b60bcfb96660ea4e
+
+
 	 }
 }
