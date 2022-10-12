573e1570fd694d193f6ea35a3f815251e8ff3cea
MarlonMartinss <marlonsd783@gmail.com>
2022-10-12 19:08:29 -0300

    atualizando

---
+++ src/application/program.java
@@ -0,0 +1,18 @@
+package application;
+
+
+import chess.ChessMatch;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+	 ChessMatch chessMatch = new ChessMatch();
+	 UI.printBoard(chessMatch.getpieces());
+	 
+	
+			
+		
+	}
+
+}
